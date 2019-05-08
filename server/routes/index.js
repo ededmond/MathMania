db17edcbe104e719f63bebf687c3d99c840e79ba
laurenkerns <laurenkerns@gmail.com>
2019-05-07 21:27:40 -0400

    some routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,16 @@ const path = require("path");
 const router = require("express").Router();
 const authRoutes = require("./auth");
 const apiRoutes = require("./api");
+const htmlRoutes = require("./html")
 
 // Auth Routes
 router.use('/auth', authRoutes);
 
 // API Routes
 router.use("/api", apiRoutes);
+
+//HTML Routes
+router.use("/html", htmlRoutes);
 
 // If no routes are hit, send the React app
 router.use(function(req, res) {
