9c4350f4a6c1d8491701c8a75069418789b8d7ab
Emma Dedmond <emmadedmond@yahoo.com>
2019-05-09 20:53:21 -0400

    block working with buttons rather than prompt

--- client/src/components/Fraction/pieces-button/index.js
+++ client/src/components/Fraction/pieces-button/index.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import "./style.css";
 const PiecesButton = props => {
-    
+    return(<div className = {props.show ? "show" : "hide"}>
+        <button onClick={props.piecesReturn} value ={2} >2</button>
+        <button onClick={props.piecesReturn} value={3}>3</button>
+    </div>)
 }
+
+export default PiecesButton;
