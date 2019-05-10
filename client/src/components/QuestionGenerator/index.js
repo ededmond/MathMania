cc842426d7974082cc09a7b66831c48ad12d7db8
Monty <tuckerjsarah@gmail.com>
2019-05-09 21:33:45 -0400

    faction fix

--- client/src/components/QuestionGenerator/index.js
+++ client/src/components/QuestionGenerator/index.js
@@ -29,17 +29,25 @@ class QuestionGenerator extends Component{
      })
      .catch(err => console.log(err))
     }
-
+    handleConvert= (arr) => {
+        let temp= arr.split('</mn><mn>').join('</sup>&frasl;<sub>');
+        let temp2= temp.split('<mfrac><mn>').join('<mfrac><sup>');
+        return temp2.split('</mn></mfrac>').join('</sub><mfrac>');
+    }
+// <math><sup>1</sup>&frasl;<sub>10</sub></math>
     render(){
-        let choices= this.state.choices.map((x,i) => <div className= "form-check" key={i}><input className= "form-check-input" type= 'radio' name= 'choicesRadio' value={i}/><label className= "form-check-label" for=  {'choicesRadio'+ i} dangerouslySetInnerHTML={{__html: x}}></label></div>)
-        let question = `<math>${this.state.question}</math>`
+        let choices= this.state.choices.map((x,i) => <div className= "form-check" key={i}><input className= "form-check-input" type= 'radio' name= 'choicesRadio' value={i}/><label className= "form-check-label" htmlFor=  {'choicesRadio'+ i} dangerouslySetInnerHTML={{__html: this.handleConvert(x)}}></label></div>)
+        let question = `<math>${this.handleConvert(this.state.question)}</math>`;
+        console.log(question)
+ 
         return(
-            <div>
+            <div>            
                 <button onClick= {this.handleQuestionPost}>Get a question</button>
-                <h1>Instructions: {this.state.instructions}</h1>
+                <h1>{this.state.instructions}</h1>
                 <h3 dangerouslySetInnerHTML={{__html: question}}></h3>
                 {choices}
                 <button onClick= {console.log(this.state.answer)}>Submit</button>
+           
             </div>
         )
     }
