c9b03a9dda5c5d87f57f0e244628ff043666f59e
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-05-13 20:18:10 -0400

    update dashboard with api call

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { render } from "react-dom"; 
+import React from "react";
 import "@devexpress/dx-react-grid"; 
 import {
   Grid, 
@@ -7,18 +6,20 @@ import {
   TableHeaderRow
 } from "@devexpress/dx-react-grid-bootstrap4"; 
 import "@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css";
-
-// import DeleteBtn from "../../components/AlperComponents/DeleteBtn";
 import Jumbotron from "../../components/AlperComponents/Jumbotron";
+import { Col, Row, Container } from "../../components/AlperComponents/Grid";
+import AUTH from "../../utils/AUTH";
+
 // import API from "../../utils/API";
 // import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/AlperComponents/Grid";
+// import DeleteBtn from "../../components/AlperComponents/DeleteBtn";
+// import { render } from "react-dom"; 
 // import { List, ListItem } from "../../components/AlperComponents/List";
 // import { Input, TextArea, FormBtn } from "../../components/AlperComponents/Form";
 
 class Dashboard extends React.PureComponent {
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
       columns: [
         { studentName: "name", title: "Student Name" },
@@ -41,44 +42,57 @@ class Dashboard extends React.PureComponent {
         }
       ],
       rows: [
-        {
-          time: "2 hours",
-          name: "Jackie",
-          questionCategory: "fractions",
-          numQuestAns: "20",
-          numCorrect: "15",
-          numWrong: "5"
-        },
-        {
-          time: "4 hours",
-          name: "Lauren",
-          Category: "fractions",
-          numQuestAns: "50",
-          numCorrect: "30",
-          numWrong: "20"
-        },
-        {
-          time: "10 hours",
-          name: "Emma",
-          Category: "fractions",
-          numQuestAns: "100",
-          numCorrect: "90",
-          numWrong: "10"
-        },
-        {
-          time: "5 hours",
-          name: "Spencer",
-          questionCategory: "fractions",
-          numQuestAns: "30",
-          numCorrect: "15",
-          numWrong: "15"
-        }
+        //   time: "2 hours",
+        //   name: "Jackie",
+        //   questionCategory: "fractions",
+        //   numQuestAns: "20",
+        //   numCorrect: "15",
+        //   numWrong: "5"
+        // },
+        // {
+        //   time: "4 hours",
+        //   name: "Lauren",
+        //   Category: "fractions",
+        //   numQuestAns: "50",
+        //   numCorrect: "30",
+        //   numWrong: "20"
+        // },
+        // {
+        //   time: "10 hours",
+        //   name: "Emma",
+        //   Category: "fractions",
+        //   numQuestAns: "100",
+        //   numCorrect: "90",
+        //   numWrong: "10"
+        // },
+        // {
+        //   time: "5 hours",
+        //   name: "Spencer",
+        //   questionCategory: "fractions",
+        //   numQuestAns: "30",
+        //   numCorrect: "15",
+        //   numWrong: "15"
+        // }
       ]
     };
   }
 
+  componentDidMount = () => {
+    AUTH.getStudents()
+      .then(response => {
+        console.log("students: ", response.data);
+        this.setState({
+          rows: response.data
+        });
+        console.log(this.state);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
-    const { rows, columns } = this.state; 
+    const { rows, columns } = this.state;
 
     return (
       <Container fluid>
