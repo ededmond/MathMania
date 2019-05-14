1d0c74c6eae4c9e52e78c2dbabc8b29905f3c988
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-05-14 18:04:46 -0400

    update dashboard to get API call

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -10,13 +10,6 @@ import Jumbotron from "../../components/AlperComponents/Jumbotron";
 import { Col, Row, Container } from "../../components/AlperComponents/Grid";
 import AUTH from "../../utils/AUTH";
 
-// import API from "../../utils/API";
-// import { Link } from "react-router-dom";
-// import DeleteBtn from "../../components/AlperComponents/DeleteBtn";
-// import { render } from "react-dom"; 
-// import { List, ListItem } from "../../components/AlperComponents/List";
-// import { Input, TextArea, FormBtn } from "../../components/AlperComponents/Form";
-
 class Dashboard extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -30,49 +23,19 @@ class Dashboard extends React.PureComponent {
         },
         {
           numberQuestionsAnswered: "numQuestAns",
-          title: "Number of Questions Answered"
+          title: "Questions Answered"
         },
         {
           numberQuestionsCorrect: "numCorrect",
-          title: "Number of Questions Correct"
+          title: "Answered Correct"
         },
         {
           numberQuestionsWrong: "numWrong",
-          title: "Number of Questions Wrong"
+          title: "Answered Incorrect"
         }
       ],
       rows: [
-        //   time: "2 hours",
-        //   name: "Jackie",
-        //   questionCategory: "fractions",
-        //   numQuestAns: "20",
-        //   numCorrect: "15",
-        //   numWrong: "5"
-        // },
-        // {
-        //   time: "4 hours",
-        //   name: "Lauren",
-        //   Category: "fractions",
-        //   numQuestAns: "50",
-        //   numCorrect: "30",
-        //   numWrong: "20"
-        // },
-        // {
-        //   time: "10 hours",
-        //   name: "Emma",
-        //   Category: "fractions",
-        //   numQuestAns: "100",
-        //   numCorrect: "90",
-        //   numWrong: "10"
-        // },
-        // {
-        //   time: "5 hours",
-        //   name: "Spencer",
-        //   questionCategory: "fractions",
-        //   numQuestAns: "30",
-        //   numCorrect: "15",
-        //   numWrong: "15"
-        // }
+
       ]
     };
   }
