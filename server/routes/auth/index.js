cd80b2930e3501472f7806a39d4c4033bf420e02
Emma Dedmond <emmadedmond@yahoo.com>
2019-05-14 22:12:14 -0400

    teacher can now update student difficulty level

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -6,6 +6,7 @@ const userController = require("../../controllers/userController");
 // this route is just used to get the user basic info
 router.get('/user', userController.getUser)
 router.get('/students',userController.getStudents);
+router.post('/students/:id',userController.changeLevel);
 router.post('/grades',userController.grade);
 router.post('/login', userController.auth, passport.authenticate('local'), userController.authenticate);
 router.post('/logout', userController.logout);
