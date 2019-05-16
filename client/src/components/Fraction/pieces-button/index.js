50ece17c95afa81a152ccbe8703ab8f44372507f
laurenkerns <laurenkerns@gmail.com>
2019-05-16 19:41:13 -0400

    fraction button spacing

--- client/src/components/Fraction/pieces-button/index.js
+++ client/src/components/Fraction/pieces-button/index.js
@@ -6,12 +6,12 @@ const PiecesButton = props => {
     //     <button onClick={props.piecesReturn} value={3}>3</button>
     // </div>)
     return <div className = {props.show ? "show" : 'hide'}>
-        <button name = "fraction-button" className = "fraction-button btn btn-success" onClick={props.piecesReturn} value ={2} >2</button>
-        <button name = "fraction-button" className = "fraction-button btn btn-success" onClick={props.piecesReturn} value={3}>3</button>
-        <button name = "fraction-button" className = "fraction-button btn btn-success" onClick={props.piecesReturn} value ={5} >5</button>
-        <button name = "fraction-button" className = "fraction-button btn btn-success" onClick={props.piecesReturn} value={7}>7</button>
-        <button name = "fraction-button" className = "fraction-button btn btn-success" onClick={props.piecesReturn} value ={11} >11</button>
-        <button name = "fraction-button" className = "fraction-button btn btn-success" onClick={props.piecesReturn} value={13}>13</button>
+        <button name = "fraction-button" className = "fraction-button btn btn-info" onClick={props.piecesReturn} value ={2} >2</button>
+        <button name = "fraction-button" className = "fraction-button btn btn-info" onClick={props.piecesReturn} value={3}>3</button>
+        <button name = "fraction-button" className = "fraction-button btn btn-info" onClick={props.piecesReturn} value ={5} >5</button>
+        <button name = "fraction-button" className = "fraction-button btn btn-info" onClick={props.piecesReturn} value={7}>7</button>
+        <button name = "fraction-button" className = "fraction-button btn btn-info" onClick={props.piecesReturn} value ={11} >11</button>
+        <button name = "fraction-button" className = "fraction-button btn btn-info" onClick={props.piecesReturn} value={13}>13</button>
     </div>
 }
 
