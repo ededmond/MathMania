8b8fd67c69b431d401b50a0bdc0f866a45a30adf
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-05-21 20:05:46 -0400

    add Jest for testing and add sum test case as an example

---
+++ server/models/student.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+  name: { type: String, required: true },
+  // username: { type: String, required: true },
+  // password: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
